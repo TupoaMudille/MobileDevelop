aa2e8696bbff7fdf9a701bed609bac6583d14ffb
TupoaMudille <71610105+TupoaMudille@users.noreply.github.com>
2023-03-21 01:36:51 +0300

    +4lab

--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lab1/labs/lab12.dart';
 import 'package:lab1/labs/lab3.dart';
+import 'package:lab1/labs/lab4.dart';
+import 'package:lab1/labs/datetimelab4.dart';
 
-void main() => runApp(Lab3());
+void main() => runApp(Lab4());
