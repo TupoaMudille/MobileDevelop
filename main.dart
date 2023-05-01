5b194ab03c1591e48a311ef161a4f21c780954da
TupoaMudille <71610105+TupoaMudille@users.noreply.github.com>
2023-05-01 20:43:04 +0300

    +lab6

--- main.dart
+++ main.dart
@@ -10,13 +10,19 @@ import 'package:lab1/labs/lab5/3.dart';
 import 'package:lab1/labs/lab5/11.dart';
 import 'package:lab1/labs/lab5/7.dart';
 import 'package:lab1/labs/lab5/10.dart';
+import 'package:lab1/labs/lab5/8.dart';
 import 'package:lab1/labs/lab5/6,8/6,8.dart';
 import 'package:lab1/labs/lab5/9/mainscreen9.dart';
+import 'package:lab1/labs/lab6/Task1.dart';
+import 'package:lab1/labs/lab6/Task2.dart';
+import 'package:lab1/labs/lab6/Task3.dart';
+import 'package:lab1/labs/lab6/Task4.dart';
+import 'package:lab1/labs/lab6/Task5.dart';
 
 //4 и 5 задания из 5 лабы есть в datetimelab4
 void main() {
   runApp(MaterialApp(initialRoute: '/', routes: {
-    '/': (BuildContext context) => MainScreen68(),
+    '/': (BuildContext context) => const Task2(),
     '/second': (BuildContext context) => SecondScreen()
   }));
 }
