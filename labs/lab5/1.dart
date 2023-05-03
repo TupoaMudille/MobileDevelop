2aac472297ad06d5971cd3bcf6de038c74d98c68
TupoaMudille <71610105+TupoaMudille@users.noreply.github.com>
2023-05-03 23:53:21 +0300

    Update 1.dart

--- labs/lab5/1.dart
+++ labs/lab5/1.dart
@@ -1,30 +1,68 @@
 import 'package:flutter/material.dart';
-
-int index = 0;
+import 'package:lab1/labs/lab5/11.dart';
 
 class MainScreen extends StatelessWidget {
+  final int _index;
+  final int _countAdd;
+  final int _countMinus;
+  MainScreen(
+      {super.key,
+      required int index,
+      required int countAdd,
+      required int countMinus})
+      : _index = index,
+        _countAdd = countAdd,
+        _countMinus = countMinus;
+
+  int tempindex = 0;
+  int tempcountAdd = 0;
+  int tempcountMinus = 0;
+
+  void initState() {
+    tempindex = _index;
+    tempcountAdd = _countAdd;
+    tempcountMinus = _countMinus;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Страница $index'),
+          title: Text('Страница $_index'),
           automaticallyImplyLeading: false,
         ),
         body: Column(children: [
-          Text('глубина стека $index'),
+          Text('глубина стека $_index'),
+          Text('сколько добавили $_countAdd'),
+          Text('сколько удалили $_countMinus'),
           TextButton(
               onPressed: () {
-                index++;
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MainScreen()));
+                initState();
+                tempindex++;
+                tempcountAdd++;
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MainScreen(
+                            index: tempindex,
+                            countAdd: tempcountAdd,
+                            countMinus: tempcountMinus)));
               },
-              child: Text('вперед')),
+              child: Text('Добавить')),
           TextButton(
-            child: Text('назад'),
+            child: Text('Удалить'),
             onPressed: () {
-              if (index > 0) {
-                index--;
-                Navigator.pop(context);
+              if (_index > 0) {
+                initState();
+                tempindex--;
+                tempcountMinus++;
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MainScreen(
+                            index: tempindex,
+                            countAdd: tempcountAdd,
+                            countMinus: tempcountMinus)));
               }
             },
           ),
